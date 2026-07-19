8fb66fc812b1505816d49ccd50ac79cdd0e65b5f
mohrip <m25srt@gmail.com>
2026-07-19 09:11:23 +0300

    feat(service) added service package into customer

---
+++ src/main/java/com/io/CoreBackend/customer/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.io.CoreBackend.customer.service;
+
+import com.io.CoreBackend.customer.dto.*;
+import com.io.CoreBackend.customer.entity.Customer;
+import com.io.CoreBackend.customer.mapper.CustomerMapper;
+import com.io.CoreBackend.customer.repository.CustomerRepository;
+import com.io.CoreBackend.shared.exception.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CustomerService {
+}
