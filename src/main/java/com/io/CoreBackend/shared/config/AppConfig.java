3ba46e341fe48aa51ed96259cd994bbf9b3aa287
mohrip <m25srt@gmail.com>
2026-07-26 11:13:27 +0300

    feat(service) added security

--- src/main/java/com/io/CoreBackend/shared/config/AppConfig.java
+++ src/main/java/com/io/CoreBackend/shared/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableJpaAuditing
-//@EnableScheduling
+@EnableScheduling
 //@EnableCaching
 public class AppConfig {
 }
