3000f19298b72969cd1236ede8d54877d203e7cb
mohrip <m25srt@gmail.com>
2026-07-26 11:34:39 +0300

    (feat): add aop module — For logging

--- src/main/java/com/io/CoreBackend/aop/LoggingAspect.java
+++ src/main/java/com/io/CoreBackend/aop/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.io.CoreBackend.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Around;
@@ -9,12 +10,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 
 @Aspect
 @Component
 public class LoggingAspect {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+
+    @Before("@within(org.springframework.stereotype.Service)")
+    public void logBeforeExecution(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        log.info("Starting execution of method: {} with arguments: {}",
+                methodName, Arrays.toString(args));
+    }
+
+
     @Around("@within(org.springframework.stereotype.Service)")
     public Object logExecution(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
