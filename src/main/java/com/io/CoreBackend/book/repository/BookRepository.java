b721f90022c5ba304ddfb362e781e1742baa62cc
mohrip <m25srt@gmail.com>
2026-07-28 10:52:08 +0300

    feat(service) added locking query on Bookid

--- src/main/java/com/io/CoreBackend/book/repository/BookRepository.java
+++ src/main/java/com/io/CoreBackend/book/repository/BookRepository.java
@@ -1,10 +1,17 @@
 package com.io.CoreBackend.book.repository;
 
 import com.io.CoreBackend.book.entity.Book;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -24,4 +31,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
     boolean existsByIsbnAndIdNot(String isbn, Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT b FROM Book b WHERE b.id IN :ids")
+    List<Book> findAllByIdForUpdate(@Param("ids") Collection<Long> ids);
 }
