42fce53e7f251342ab0ad03d7eb57d5ef745f927
mohrip <m25srt@gmail.com>
2026-07-28 10:06:00 +0300

    build: prepare the  application for production

---
+++ src/main/java/com/io/CoreBackend/order/entity/Order.java
@@ -0,0 +1,13 @@
+//package com.io.CoreBackend.order.entity;
+//
+//import com.io.CoreBackend.customer.entity.Customer;
+//import com.io.CoreBackend.shared.entity.BaseEntity;
+//import jakarta.persistence.*;
+//import java.math.BigDecimal;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "orders")
+//public class Order {
+//}
