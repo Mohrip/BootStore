479d998fc0edaa27e31ca4add1d9f0af41e655af
mohrip <m25srt@gmail.com>
2026-07-29 09:47:51 +0300

    feat(service) added Repository for cart service

---
+++ src/main/java/com/io/CoreBackend/order/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.io.CoreBackend.order;
+
+import com.io.CoreBackend.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @EntityGraph(attributePaths = {"items"})
+    Page<Order> findByCustomerIdOrderByCreatedAtDesc(Long customerId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"items"})
+    Optional<Order> findByIdAndCustomerId(Long orderId, Long customerId);
+}
