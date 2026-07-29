02796e1c7cfcd2c90a5f5b15482e87b45240bf99
mohrip <m25srt@gmail.com>
2026-07-29 09:56:58 +0300

    feat(service) added mapper for cart service

---
+++ src/main/java/com/io/CoreBackend/order/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.io.CoreBackend.order.mapper;
+
+import com.io.CoreBackend.order.dto.*;
+import com.io.CoreBackend.order.entity.*;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class OrderMapper {
+}
